1db023f6c22bbcba622c8f3e5fd1292d20b81a83
B00165945 Gavril Marian <B00165945@mytudublin.ie>
2025-10-06 16:45:20 +0100

    Finished Part 2 Add the StyleSheet

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ export default function App() {
   
   
   function clickMe(){
+    let totalGradeScores = 0;
     alert("this is the click me button"); //alert for web
     Alert.alert("this is the click me button"); //alert for phone
     //get the studnet's gradePointsfor SSWD, multiply it by the credits 
@@ -32,21 +33,59 @@ export default function App() {
     //Output the calculated GPA result to the user using an alert (you must concatenate the gpa)
    alert("gpa = " + gpa.toFixed(2));
   }
-
+  const styles = StyleSheet.create({
+    container: {
+      padding: "5%",
+    },
+    row: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginBottom: "5%",
+    },
+    label: {
+      flex: 1,
+      marginRight: "5%",
+      fontWeight: "bold",
+    },
+    textInput: {
+      flex: 1,
+      borderColor: '#ccc',
+      borderWidth: 1,
+      padding: "3%",
+      marginLeft: "5%",
+      marginRight: "5%",
+      borderRadius: 4,
+    }
+  });
 
 return (
-    <SafeAreaView>
-      <View><Text style={{flexDirection: "row", fontWeight: "bold", fontSize: 24, textAlign:"center", marginTop: "%10"}}>GPA Calculator</Text></View>
-      <View>
-        <Text>Maths</Text>
-        <TextInput  placeholder="Grade" onChangeText={setSswd}/>
+     <SafeAreaView style={styles.container}>
+      <View style={{ marginBottom: "10%" }}>
+        <Text style={{ fontWeight: "bold", fontSize: 24, textAlign: "center" }}>GPA Calculator</Text>
       </View>
-      <View >
-        <Text>Organisational Behaviour</Text>
-        <TextInput placeholder="Grade" onChangeText={setOb}/>
+
+      <View style={styles.row}>
+        <Text style={styles.label}>Maths</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Grade"
+          onChangeText={text => setSswd(text.toUpperCase())}
+          value={sswd}
+        />
       </View>
-      <View >
-        <Button title="submit" onPress={clickMe}/>
+
+      <View style={styles.row}>
+        <Text style={styles.label}>Organisational Behaviour</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Grade"
+          onChangeText={text => setOb(text.toUpperCase())}
+          value={ob}
+        />
+      </View>
+
+      <View style={{ marginTop: "5%" }}>
+        <Button title="Submit" onPress={clickMe} />
       </View>
     </SafeAreaView>
   );
